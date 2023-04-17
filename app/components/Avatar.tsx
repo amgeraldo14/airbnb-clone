2ff58af3cd447a3b512c04a11f54251bd732e61f
aldo <aldo@jjindonesia.com>
2023-04-17 14:25:19 +0700

    add google and github signup

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,15 +2,20 @@
 
 import React from "react";
 import Image from "next/image";
+import { SafeUser } from "../types";
 
-const Avatar = () => {
+interface AvatarProps {
+  src?: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       height={30}
       width={30}
       alt="avatar"
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
     />
   );
 };
